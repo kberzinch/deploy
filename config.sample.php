4bb070764c7a0536fa7d909a023577b47dc3b5ed
Kristaps Berzinch <kristapsberzinch@gmail.com>
2017-06-23 20:17:39 -0500

    Updated config sample

--- config.sample.php
+++ config.sample.php
@@ -1,14 +1,31 @@
 <?php
 
-// Copy me to config.php and replace the secret below with the one you actually used
-$secret = "the same secret you put in the GitHub webhook configuration";
+///////////////////////////////////////////////////////////
+// Copy this file to config.php and edit as appropriate. //
+///////////////////////////////////////////////////////////
 
-// Uncomment and fill in BOTH of these with email addresses to send an email when the script runs.
-// $email_from = "";
-// $email_to = "";
+/**
+ * Set $webhook_secret to the same value you entered in the GitHub webhook
+ * configuration.
+ */
+$webhook_secret = 'generate me at randomkeygen.com or wherever';
 
-// If you want deploy to set up new private repositories for you and you don't have credentials stored another way,
-// put your authorization string below in the format "username:password@"; otherwise leave it blank.
-$auth = "";
+/**
+ * If provided below, a personal access token will be used to clone new
+ * repositories and publish deployment status information to the GitHub API.
+ *
+ * Generate a personal access token here: https://github.com/settings/tokens
+ * If you want to be fancy, you can also get a real OAuth token. This is left
+ * as an exercise for the reader.
+ *
+ * You can also add GitHub Enterprise credentials using the hostname in a
+ * similar format.
+ */
+//$token['github.com'] = 'f6609dbf9796004709d3c34e7abbad8fb4a737ad';
 
-$printoutput = true;
+/**
+ * If you'd like email notifications when the script runs, set the two
+ * variables below.
+ */
+//$email_from = 'deploy@example.com';
+//$email_to = 'you@example.com';
