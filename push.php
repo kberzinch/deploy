732a96aa0280fe7b5db820538b2ca0e2d4370d34
Kristaps Berzinch <kristapsberzinch@gmail.com>
2017-07-01 14:44:20 -0500

    Diagnostic info for push hook

--- push.php
+++ push.php
@@ -7,5 +7,10 @@ $payload = payload();
 
 // Trigger a deployment if the branch matches
 if ('refs/heads/'.$_GET["branch"] === $payload["ref"]) {
+    echo "Deployment triggered."
     trigger_deployment($_GET["environment"]);
+} else {
+    echo "Not triggering deployment\n".
+    "Target branch: ".$_GET["branch"]."\n".
+    "This ref:      ".$payload["ref"];
 }
