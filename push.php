9bf56ab52b1ab91fc167ed89b9bbcffa3612f309
Kristaps Berzinch <kristapsberzinch@gmail.com>
2017-07-01 14:44:54 -0500

    Missed a semicolon

--- push.php
+++ push.php
@@ -7,7 +7,7 @@ $payload = payload();
 
 // Trigger a deployment if the branch matches
 if ('refs/heads/'.$_GET["branch"] === $payload["ref"]) {
-    echo "Deployment triggered."
+    echo "Deployment triggered.";
     trigger_deployment($_GET["environment"]);
 } else {
     echo "Not triggering deployment\n".
