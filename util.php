dfd8443bb83a363d6edefd1de60dc0ac4777544e
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-12-27 15:56:40 -0600

    Use in_progress state

--- util.php
+++ util.php
@@ -131,13 +131,13 @@ function set_status($state, $description)
             "log_url" => "https://".$_SERVER["SERVER_NAME"]."/"
                 .$payload["repository"]["name"]."/".$payload["deployment"]["environment"]."/"
                 .$payload["deployment"]["sha"]."/".$payload["deployment"]["id"]
-                .($state === "pending" ? "/" : "/plain.txt"),
+                .($state === "in_progress" ? "/" : "/plain.txt"),
             "description" => $description
         ],
         "setting status",
         "application/vnd.github.ant-man-preview+json, application/vnd.github.flash-preview+json"
     );
-    if ($state !== "pending") {
+    if ($state !== "in_progress") {
         file_put_contents($log_location."/plain.txt", "\n# ".$description."\n", FILE_APPEND);
     }
 }
