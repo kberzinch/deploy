14635c1067305b9aa618036b27560d9b70ec66c6
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-12-27 20:28:21 -0600

    Add app manifest (probably works?)

---
+++ public/setup.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists(__DIR__."/../config.php")) {
+    exit(file_get_contents(__DIR__."/../config_exists.html"));
+}
+
+exit(file_get_contents(__DIR__."/../setup_wizard.html"));
